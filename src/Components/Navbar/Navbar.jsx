7b5dbafc8dfc02812c3494a01cf94c48b681ed20
Anshu010412 <Anshujhacoc@gmail.com>
2026-02-14 14:30:27 +0530

    fix bugs

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -37,9 +37,8 @@ const Navbar = () => {
 
   return (
     <nav
-     className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] ${
-        isScrolled ? "bg-[#050414]/20 bg-opacity-50 backdrop-blur-md shadow-md" : "bg-transparent"
-      }`}
+      className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] ${isScrolled ? "bg-[#050414]/20 bg-opacity-50 backdrop-blur-md shadow-md" : "bg-transparent"
+        }`}
     >
       <div className="text-white py-5 flex justify-between items-center">
 
