ffca98875e961414a3280ad879853098e0e5e250
Anshu010412 <Anshujhacoc@gmail.com>
2026-02-27 00:56:59 +0530

    fix link

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1HlQ94lijmdAwkVxnmBWeojiXSXMkN_4J/view"
+            href="https://drive.google.com/file/d/1Gd2SoQu7Fue3qJgBEMUvzWV0fmccX-77/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 hover:scale-105"
