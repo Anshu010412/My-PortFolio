412a5a366dfb859086494d5e7f9795c639fea47d
Anshu010412 <Anshujhacoc@gmail.com>
2026-02-27 01:07:14 +0530

    fix link

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1Gd2SoQu7Fue3qJgBEMUvzWV0fmccX-77/view?usp=sharing"
+            href="../../assets/Anshu_Resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 hover:scale-105"
